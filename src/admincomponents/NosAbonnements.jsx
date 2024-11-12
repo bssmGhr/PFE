08256837586d3181d8b3559d5eec4aaf6125c345
bssmGhr <bssmghr44@gmail.com>
2024-11-12 13:31:41 +0100

    m

---
+++ src/admincomponents/NosAbonnements.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NosAbonnements() {
+    return (
+        <header class="bg-primary text-white text-center py-5">
+            <h1>Nos Abonnements</h1>
+            <p>Choisissez le forfait qui vous convient et commencez votre parcours fitness dès aujourd'hui !
+            </p>
+        </header>
+    )
+}
+
+export default NosAbonnements
