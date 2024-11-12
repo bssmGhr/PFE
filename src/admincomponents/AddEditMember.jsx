91993bcbe49c5517f6f212d17de30bd769ff6caf
bssmGhr <bssmghr44@gmail.com>
2024-11-12 13:28:39 +0100

    m

---
+++ src/admincomponents/AddEditMember.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AddEditMember() {
+    return (
+        <section class="text-center my-5">
+            <h2>Add/Edit Member</h2>
+            <form id="member-form">
+                <div class="mb-3">
+                    <input type="hidden" id="member-id" />
+                    <input type="text" id="member-name" class="form-control" placeholder="Member Name" required />
+                </div>
+                <button type="submit" class="btn btn-primary" id="submit-btn">Add Member</button>
+            </form>
+        </section>
+    )
+}
+
+export default AddEditMember
