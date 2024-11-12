c24d3dfab785f2207057016ace72dc53cec57d2f
bssmGhr <bssmghr44@gmail.com>
2024-11-12 12:14:16 +0100

    m

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Services from './components/Services'
+import WhyChooseUs from './components/WhyChooseUs'
+import Offers from './components/Offers'
+import Testimonials from './components/Testimonials'
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <h1>Home</h1>
+            <Services />
+            <WhyChooseUs />
+            <Offers />
+            <Testimonials />
+        </div>
     )
 }
 
