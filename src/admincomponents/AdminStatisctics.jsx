d86045fbb261033b5356d2768020159badea8712
bssmGhr <bssmghr44@gmail.com>
2024-11-12 13:28:04 +0100

    m

---
+++ src/admincomponents/AdminStatisctics.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AdminStatisctics() {
+    return (
+        <section class="row text-center my-5">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h3>Total Members</h3>
+                        <p id="total-members" class="display-4">0</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AdminStatisctics
