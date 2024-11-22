cc6dad815e7bad7331a152f00ce0d6d21de6faef
bssmGhr <bssmghr44@gmail.com>
2024-11-22 10:50:51 +0100

    m

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,16 +12,16 @@ function Contact() {
             <form class="contact-form">
                 <label for="name">Name:</label>
                 <input type="text" id="name" name="name" required />
-
+                <br />
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email" required />
-
+                <br />
                 <label for="phone">Phone Number:</label>
                 <input type="tel" id="phone" name="phone" required />
-
+                <br />
                 <label for="message">Message:</label>
                 <textarea id="message" name="message" rows="5" required></textarea>
-
+                <br />
                 <button type="submit">Submit</button>
             </form>
         </section>
