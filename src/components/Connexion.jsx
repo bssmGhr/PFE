05c01c233f8ee469d39026104393870b1fa7b5fe
bssmGhr <bssmghr44@gmail.com>
2024-11-22 11:10:31 +0100

    m

--- src/components/Connexion.jsx
+++ src/components/Connexion.jsx
@@ -17,11 +17,11 @@ function Connexion() {
             <h2 className="text-center mb-4">Login</h2>
             <form id="login-form" onsubmit={(e) => validateLogin(e)}>
                 <div className="mb-3">
-                    <label for="login-email" className="form-label">Email:</label>
+                    <label htmlFor="login-email" className="form-label">Email:</label>
                     <input type="email" className="form-control" id="login-email" name="email" required />
                 </div>
                 <div className="mb-3">
-                    <label for="login-password" className="form-label">Password:</label>
+                    <label htmlFor="login-password" className="form-label">Password:</label>
                     <input type="password" className="form-control" id="signup-password" name="password" required minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}" />
                     <div className="invalid-feedback">
                         Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.
