541e85999564cf0dfc7f5500ab2916dc7f16ade6
bssmGhr <bssmghr44@gmail.com>
2024-11-22 11:26:39 +0100

    m

--- src/admincomponents/Admin.jsx
+++ src/admincomponents/Admin.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AdminTrainers from './AdminTrainers'
 import AdminMembersTable from './AdminMembersTable'
+import AdminNosAbonnements from './AdminNosAbonnements'
 function Admin() {
     return (
         <div>Admin
             <AdminMembersTable />
             <AdminTrainers />
+            <AdminNosAbonnements />
 
         </div>
     )
