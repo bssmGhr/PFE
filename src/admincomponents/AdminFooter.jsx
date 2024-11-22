78dd17117c6a16c51753efb6332df028e04b4793
bssmGhr <bssmghr44@gmail.com>
2024-11-22 12:47:29 +0100

    m

--- src/admincomponents/AdminFooter.jsx
+++ src/admincomponents/AdminFooter.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import '../styles/AdminFooter.css'
+import { Link } from 'react-router-dom'
 function AdminFooter() {
     return (
-        <footer className="text-center py-4 bg-dark text-light">
+        <footer className="text-center py-4">
             <h3>Total Members</h3>
             <p id="total-members" className="display-4">0</p>
 
-            <p>&copy;GO TO GYM Admin</p>
+            <p>&copy; 2024 GO TO GYM Admin. All rights reserved.</p> <p> <Link to="/PrivacyPolicy">Privacy Policy</Link> | <Link to="/TermsOfService">Terms of Service</Link> | <Link to="/ContactUs">Contact Us</Link> </p>
+
 
         </footer>
     )
