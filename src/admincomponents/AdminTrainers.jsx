2ef6569ba8bb555052ff0ce3d4eb6727037089df
bssmGhr <bssmghr44@gmail.com>
2024-11-22 11:46:14 +0100

    m

--- src/admincomponents/AdminTrainers.jsx
+++ src/admincomponents/AdminTrainers.jsx
@@ -3,28 +3,29 @@ import React from 'react'
 function AdminTrainers() {
     return (
         <section id="Trainers">
-            <h2>Nos Coaches</h2>
+            <h2>Our Coaches</h2>
             <div className="trainer">
                 <img src="coach1.jpg" alt="Coach 1" />
                 <h3>Jean Dupont</h3>
-                <p>Spécialité : Musculation et CrossFit</p>
+                <p>Specialty: Weightlifting and CrossFit</p>
             </div>
             <div className="trainer">
                 <img src="" alt="Coach 1" />
                 <h3>Jean Dupont</h3>
-                <p>Spécialité : Musculation et CrossFit</p>
+                <p>Specialty: Weightlifting and CrossFit</p>
             </div>
             <div className="trainer">
                 <img src="" alt="Coach 2" />
                 <h3>Marie Leclerc</h3>
-                <p>Spécialité : Yoga et Cardio</p>
+                <p>Specialty: Yoga and Cardio</p>
             </div>
             <div className="trainer">
                 <img src="coach3.jpg" alt="Coach 3" />
                 <h3>David Martin</h3>
-                <p>Spécialité : HIIT et Perte de Poids</p>
+                <p>Specialty: HIIT and Weight Loss</p>
             </div>
         </section>
+
     )
 }
 
