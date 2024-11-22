f7b0ca6b75194fdb1d1ecce5896d8907f817b12e
bssmGhr <bssmghr44@gmail.com>
2024-11-22 11:09:09 +0100

    m

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,16 +10,16 @@ function Contact() {
 
             <h3>Send Us a Message</h3>
             <form className="contact-form">
-                <label for="name">Name:</label>
+                <label htmlFor="name">Name:</label>
                 <input type="text" id="name" name="name" required />
                 <br />
-                <label for="email">Email:</label>
+                <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" required />
                 <br />
-                <label for="phone">Phone Number:</label>
+                <label htmlFor="phone">Phone Number:</label>
                 <input type="tel" id="phone" name="phone" required />
                 <br />
-                <label for="message">Message:</label>
+                <label htmlFor="message">Message:</label>
                 <textarea id="message" name="message" rows="5" required></textarea>
                 <br />
                 <button type="submit">Submit</button>
