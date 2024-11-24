4668fd016fcb9834c6ba591178eb6297b16845f9
bssmGhr <bssmghr44@gmail.com>
2024-11-24 12:10:01 +0100

    m

---
+++ src/components/PolitiqueDeConfidentialite.jsx
@@ -0,0 +1,136 @@
+
+import React from 'react';
+
+function PolitiqueDeConfidentialite() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        Privacy policy of Salle de sport. This Privacy Policy describes how your personal information is collected, used, and shared when you visit https://salondechat.com (the "Site") or make a purchase there.
+      </p>
+
+      <h2>Personal Information Collected</h2>
+      <p>
+        When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically collected information as "Device Information."
+      </p>
+      <p>
+        We collect Device Information using the following technologies:
+      </p>
+      <ul>
+        <li><strong>Cookies</strong>: Here is a list of cookies that we use. We have listed them here so you can choose whether you want to allow them or not.</li>
+      </ul>
+      <ul>
+        <li>_session_id, unique session identifier, allows Shopify to store information about your session (referrer, landing page, etc.).</li>
+        <li>_shopify_visit, no data held, persists for 30 minutes from the last visit. Used by our website provider's internal statistics tracker to record the number of visits.</li>
+        <li>_shopify_uniq, no data held, expires at midnight (depending on the visitor's location) the next day. Counts the number of visits to a store by a single customer.</li>
+        <li>cart, unique identifier, persists for 2 weeks, stores information about your shopping cart.</li>
+        <li>_secure_session_id, unique session identifier</li>
+        <li>storefront_digest, unique identifier, indefinite if the store has a password, it is used to determine if the current visitor has access.</li>
+      </ul>
+      <ul>
+        <li>"Log files" track Site activity and collect data such as your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</li>
+        <li>"Web beacons," "tags," and "pixels" are electronic files used to record information about how you browse the Site.</li>
+      </ul>
+
+
+
+      <p>
+        Furthermore, when you make or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including your credit card numbers [[INSERT OTHER ACCEPTED PAYMENT METHODS, IF APPLICABLE]]), email address, and phone number. These automatically collected pieces of information are referred to as "Order Information."
+      </p>
+      <p>
+        OFFLINE DATA, DATA/LISTS PURCHASED FOR MARKETING PURPOSES
+      </p>
+      <p>
+        When we use the term "Personal Information" in this Privacy Policy, we are referring to both Device Information and Order Information.
+      </p>
+
+      <h2>How Do We Use Your Personal Information?</h2>
+      <p>
+        Generally, we use the Order Information that we collect to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to:
+      </p>
+      <ul>
+        <li>communicate with you;</li>
+        <li>screen our orders for potential risk or fraud; and</li>
+        <li>when in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</li>
+      </ul>
+
+      <p>
+
+      </p>
+      <p>
+        We use the Device Information (in particular, your IP address) that we collect to assess potential fraud or risks and, more generally, to improve and optimize our Site (for example, by generating analytics on how our customers browse and interact with the Site, and to evaluate the success of our advertising and marketing campaigns).
+      </p>
+      <p>
+        PARTICULARLY IN TERMS OF ADVERTISING AND RETARGETING
+      </p>
+
+      <h2>Sharing Your Personal Information</h2>
+      <p>
+        We share your Personal Information with third parties who help us use it for the purposes described above. For example, we use Shopify to host our online store – to learn more about how Shopify uses your Personal Information, please visit the following page: <a href="https://www.shopify.fr/legal/confidentialite">https://www.shopify.fr/legal/confidentialite</a>. We also use Google Analytics to better understand how our customers use the Site – to learn more about how Google uses your Personal Information, please visit the following page: <a href="https://www.google.com/intl/fr/policies/privacy/">https://www.google.com/intl/fr/policies/privacy/</a>. You can also opt out of Google Analytics here: <a href="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a>.
+      </p>
+      <p>
+        Finally, we may also share your Personal Information to comply with applicable laws and regulations, respond to a subpoena, search warrant, or other lawful requests for information we receive, or to protect our rights.
+      </p>
+      <p>
+
+      </p>
+
+      <h2>Behavioral Advertising</h2>
+      <p>
+        As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you. For more information on how targeted advertising works, you can visit the Network Advertising Initiative's (NAI) educational page at the following address: <a href="http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work">http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work</a>.
+      </p>
+      <p>
+        You can opt out of targeted advertising here:
+      </p>
+      <ul>
+        <li><a href="https://www.facebook.com/settings/?tab=ads">FACEBOOK</a></li>
+        <li><a href="https://www.google.com/settings/ads/anonymous">GOOGLE</a></li>
+        <li><a href="https://about.ads.microsoft.com/fr-fr/ressources/politiques/annonces-personnalisees">BING</a></li>
+      </ul>
+      <p>
+        Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance's opt-out portal at the following address: <a href="https://optout.aboutads.info/?c=3&lang=fr">https://optout.aboutads.info/?c=3&lang=fr</a>.
+      </p>
+
+
+      <h2>Do Not Track</h2>
+      <p>
+        Please note that we do not alter our Site's data collection and usage practices when we detect a "Do Not Track" signal.
+      </p>
+      <p>
+        Please note that we do not alter our Site's data collection and usage practices when we detect a "Do Not Track" signal on your browser.
+      </p>
+
+      <h2>Your Rights</h2>
+      <p>
+        If you are a European resident, you have the right to access the personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us using the contact details provided below.<br />
+        Additionally, if you are a European resident, we note that we are processing your information in order to fulfill contracts we might have with you (for example, if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above. Please also note that your information will be transferred outside of Europe, including to Canada and the United States.
+      </p>
+
+      <h2>Data Retention</h2>
+      <p>
+        When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.
+      </p>
+
+      <h2>Minors</h2>
+      <p>
+        The Site is not intended for individuals under the age of 18.
+      </p>
+
+      <h2>Changes</h2>
+      <p>
+        We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal, or regulatory reasons.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        To learn more about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by email at contact@salledesport.com, or by mail at the following address:
+        Sousse, Sousse, Sousse, 4060, Tunisia
+        Powered by Shopify
+        www.shopify.fr
+      </p>
+
+    </div >
+  )
+}
+
+export default PolitiqueDeConfidentialite
