9cdb27d5049bc9ba44eb5712fb1768978bb87b0f
bssmGhr <bssmghr44@gmail.com>
2024-11-24 12:19:23 +0100

    m

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from "react-router-dom"
 function Footer() {
     return (
         <footer className="bg-dark text-white py-4">
@@ -7,6 +7,12 @@ function Footer() {
                 <div className="row">
                     <div className="col-md-6">
                         <p>&copy; 2024 [GO TO GYM]. All rights reserved.</p>
+                    </div>
+                    <div className="col-md-6">
+                        <Link to="/privacypolicy">The Privacy Policy</Link>
+                    </div>
+                    <div className="col-md-6">
+                        <Link to="/termsofservice">Terms of Service</Link>
                     </div>
                     <div className="col-md-6">
                         <ul className="list-inline">
