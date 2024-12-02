0345861731fb8458bed59bf7e5003160cf15765a
unknown <bssmghr44@gmail.com>
2024-12-02 13:36:33 +0100

    m

--- src/admincomponents/Admin.jsx
+++ src/admincomponents/Admin.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 import AdminTrainers from './AdminTrainers'
 import AdminMembersTable from './AdminMembersTable'
 import AdminNosAbonnements from './AdminNosAbonnements'
-import PaymentSection from "./PaymentSection"
-import AdminAttendanceSection from './AdminAttendanceSection'
-import AdminCurrentAttendance from "./AdminCurrentAttendance"
+
 function Admin() {
     return (
         <div>Admin
             <AdminMembersTable />
             <AdminTrainers />
             <AdminNosAbonnements />
-            {/*<AdminCurrentAttendance />*/}
-            {/*<PaymentSection />*/}
-            {/*<AdminAttendanceSection />*/}
+            
         </div>
     )
 }
