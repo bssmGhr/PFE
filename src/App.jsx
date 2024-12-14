e8c69dfcdd372fdce8d764476c034d03f5236c29
walid ben abdessalem <benabdesslem.walid@gmail.com>
2024-12-14 14:18:15 +0100

    fix pages and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,37 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 
 import Header from './components/Header';
-import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
 import './styles/style.css';
-import Home from './components/Home'
+import Home from './pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Contact from './components/Contact';
-import Plans from './components/Plans';
-import Inscription from './components/Inscription';
-import Connexion from './components/Connexion';
-import Admin from "./admincomponents/Admin";
-import AdminAddEditMember from './admincomponents/AdminAddEditMember';
-import AdminHeader from "./admincomponents/AdminNavbar"
-import AdminFooter from "./admincomponents/AdminFooter"
-import PaymentSection from "./admincomponents/PaymentSection"
-import AdminAttendanceSection from './admincomponents/AdminAttendanceSection';
-import AdminCurrentAttendanceSection from "./admincomponents/AdminCurrentAttendance"
-import AdminTermsOfService from './admincomponents/MentionsLegales';
-import AdminPolitiqueDeConfidentialite from './admincomponents/PolitiqueDeConfidentialite';
-import TermsOfService from './components/MentionsLegales';
-import PolitiqueDeConfidentialite from './components/PolitiqueDeConfidentialite';
+import Contact from './pages/Contact';
+import Plans from './pages/Plans';
+import Inscription from './pages/Inscription';
+import Connexion from './pages/Connexion';
+import TermsOfService from './pages/MentionsLegales';
+import PolitiqueDeConfidentialite from './pages/PolitiqueDeConfidentialite';
 
 
 const App = () => {
-    const location = useLocation();
-    const isAdminRoute = location.pathname.startsWith('/admin');
-    console.log(isAdminRoute)
     return (
-        <div>
-            {isAdminRoute ? <AdminHeader /> : <Header />}
-            <h1>Hello, React with Vite!</h1>
-            <HeroSection />
+        <Router>
+            <Header></Header>
+
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/index" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/plans" element={<Plans />} />
                 <Route path="/inscription" element={<Inscription />} />
                 <Route path="/connexion" element={<Connexion />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/adminaddeditmember" element={<AdminAddEditMember />} />
-                <Route path="/adminattendancesection" element={<AdminAttendanceSection />} />
-                <Route path="/admincurrentattendance" element={<AdminCurrentAttendanceSection />} />
-                <Route path="/adminpaymentsection" element={<PaymentSection />}></Route>
-                <Route path="/adminprivacypolicy" element={<AdminPolitiqueDeConfidentialite />}></Route>
-                <Route path="/admintermsofservice" element={<AdminTermsOfService />}></Route>
                 <Route path="/privacypolicy" element={<PolitiqueDeConfidentialite />}></Route>
                 <Route path="/termsofservice" element={<TermsOfService />}></Route>
 
             </Routes>
-            {isAdminRoute ? <AdminFooter /> : <Footer />}
-        </div>
+            <Footer></Footer>
+        </Router>
     );
 };
 
-const MainApp = () => (
-    <Router>
-        <App />
-    </Router>
-);
-
-export default MainApp;
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,25 +6,13 @@ function Header() {
         <header className="bg-dark text-white py-3">
             <div className="container">
                 <div className="d-flex justify-content-between align-items-center">
-                    <Link to="index" className="d-flex align-items-center text-white text-decoration-none">
+                    <Link to="/" className="d-flex align-items-center text-white text-decoration-none">
                         <img src={imagesjfif} alt="Gym Name Logo" width="40" height="40" />
                         <span className="ms-2">[Gym Name]</span>
                     </Link>
-                    <nav className="d-none d-lg-block">
-                        <ul className="nav">
-                            <li className="btn btn-outline-light me-2">
-                                <Link to="index" className="nav-link text-white">Home</Link>
-                            </li>
-                            <li className="btn btn-outline-light me-2">
-                                <Link to="contact" className="nav-link text-white">Contact</Link>
-                            </li>
-                            <li className="btn btn-outline-light me-2">
-                                <Link to="plans" className="nav-link text-white">Plans</Link>
-                            </li>
-
-                        </ul>
-                    </nav>
                     <div className="d-flex">
+                        <Link className="btn btn-outline-light me-2" to="plans">Plans</Link>
+                        <Link className="btn btn-outline-light me-2" to="contact">Contact</Link>
                         <Link className="btn btn-outline-light me-2" to="inscription">Login</Link>
                         <Link className="btn btn-warning" to="inscription">Sign-Up</Link>
                     </div>
