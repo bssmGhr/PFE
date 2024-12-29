30c34e121b741d2346a0174ce092774c501c6a43
unknown <bssmghr44@gmail.com>
2024-12-29 14:03:58 +0100

    m

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,8 +10,8 @@ function Testimonials() {
                         <div className="p-4 bg-white rounded shadow-sm h-100">
                             <blockquote className="blockquote">
                                 <p>
-                                    “The trainers here are incredible! I've never felt more
-                                    supported in my fitness journey.”
+                                    "The trainers here are incredible! I've never felt more
+                                    supported in my fitness journey."
                                 </p>
                                 <footer className="blockquote-footer">John D.</footer>
                             </blockquote>
@@ -21,8 +21,8 @@ function Testimonials() {
                         <div className="p-4 bg-white rounded shadow-sm h-100">
                             <blockquote className="blockquote">
                                 <p>
-                                    “Joining the group fitness classes has been a game changer for
-                                    me—I've lost weight and gained confidence!”
+                                    "Joining the group fitness classes has been a game changer for
+                                    me—I've lost weight and gained confidence!"
                                 </p>
                                 <footer className="blockquote-footer">Emily R.</footer>
                             </blockquote>
@@ -32,8 +32,8 @@ function Testimonials() {
                         <div className="p-4 bg-white rounded shadow-sm h-100">
                             <blockquote className="blockquote">
                                 <p>
-                                    “Thanks to the personal training at [GO TO GYM], I've achieved
-                                    results I didn't think were possible.”
+                                    "Thanks to the personal training at [GO TO GYM], I've achieved
+                                    results I didn't think were possible."
                                 </p>
                                 <footer className="blockquote-footer">Michael</footer>
                             </blockquote>
